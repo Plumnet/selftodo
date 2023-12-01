91cdc4800ff07910b89398196103b4335f41f36b
Plumnet <tomomi0131@ymail.ne.jp>
2023-12-02 05:01:45 +0900

    追加ボタンまで見栄えが出来たソース

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import "./App.css";
 
 export const App = () => {
+  const InputForm = () => {
+
+  };
+  
+  
   return (
     <div className="App">
+
+
  
  <ul>
   <li>
-    <span>タイトル</span>
+    <span>TODO</span>
     <button>編集</button>
     <button>削除</button>
   </li>
 </ul>
+<>
+      <input
+        type='text'
+        label='タイトル'
+      />
+      <button>作成</button>
+    </>
     </div>
+    
   );
 }
+
+
