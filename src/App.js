fabc1e8fb41247a1f4d9318a9f11be1954c5536c
Plumnet <tomomi0131@ymail.ne.jp>
2023-12-02 06:30:56 +0900

    試行錯誤1(追加ボタンが表示されない)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
+
+import React from 'react';
 import "./App.css";
 
 export const App = () => {
-  const InputForm = () => {
+      return (
+        <div>
+           <ul>
+            <li>
+              <span>TODO</span>
+              <button>編集</button>
+              <button>削除</button>
+            </li>
+           </ul>
+        </div>
+      );
+};
+  
+export default App;
 
-  };
-  
-  
-  return (
-    <div className="App">
 
 
  
- <ul>
-  <li>
-    <span>TODO</span>
-    <button>編集</button>
-    <button>削除</button>
-  </li>
-</ul>
-<>
-      <input
-        type='text'
-        label='タイトル'
-      />
-      <button>作成</button>
-    </>
-    </div>
-    
-  );
-}
-
 
---
+++ src/Input.js
@@ -0,0 +1,13 @@
+export const InputForm = () => {
+    return (
+    <>
+      <input
+        type='text'
+        label='タイトル'
+      />
+      <button>作成</button>
+    </>
+    );
+  };
+
+export default InputForm;
